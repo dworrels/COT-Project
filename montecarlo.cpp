8d19923fa8a1979ca52a159d19d00c8bdbd20d4d
dworrels <dworrels1@gmail.com>
2025-04-24 22:02:04 -0400

    Create montecarlo.cpp

---
+++ montecarlo.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+// Pull functions from main chatbot file
+#define MONTE
+#include "final.cpp"
+#undef MONTE
+
+int main()
+{
+    srand((unsigned)time(nullptr));
+    // Initialize
+    startProccess();
+    stateNode node = findNode(1);
+
+    long TRIALS = 1000000;
+    long counts[3] = {0,0,0};
+
+    // Run trials
+    for (long i = 0; i < TRIALS; ++i)
+    {
+        ostringstream oss;
+        streambufold = cout.rdbuf(oss.rdbuf());
+        out_put(node);
+        cout.rdbuf(old);
+        string resp = oss.str();
+
+        // Checks response with each state, counts for which it matches.
+        if (resp == node->response[0])
+            ++counts[0];
+        else if (resp == node->response[1])
+            ++counts[1];
+        else
+            ++counts[2];
+    }
+
+    // Output results
+    cout << "Response 1: " << counts[0]
+         << " (" << counts[0]100.0/TRIALS << "%)\n"
+         << "Response 2: " << counts[1]
+         << " (" << counts[1]100.0/TRIALS << "%)\n"
+         << "Response 3: " << counts[2]
+         << " (" << counts[2]*100.0/TRIALS << "%)\n";
+
+    return 0;
+}
